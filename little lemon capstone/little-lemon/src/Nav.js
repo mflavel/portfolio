ce61a12c4d5e0d82117f310523f8e910675c1a7e
mf779 <flavelmm@clarkson.edu>
2025-09-17 09:57:47 -0400

    Nav css styling

--- little lemon capstone/little-lemon/src/Nav.js
+++ little lemon capstone/little-lemon/src/Nav.js
@@ -3,15 +3,15 @@ import logo from "./images/Logo .svg";
 
 const Nav = () => {
     return (
-        <nav>
+        <nav className="Nav">
             <img src={logo} alt="Logo" />
             <ul>
-                <li><a href="url">Home</a></li>
-                <li><a href="url">About</a></li>
-                <li><a href="url">Menu</a></li>
-                <li><a href="url">Reservations</a></li>
-                <li><a href="url">Order Online</a></li>
-                <li><a href="url">Login</a></li>
+                <li><a href="url"><b>Home</b></a></li>
+                <li><a href="url"><b>About</b></a></li>
+                <li><a href="url"><b>Menu</b></a></li>
+                <li><a href="url"><b>Reservations</b></a></li>
+                <li><a href="url"><b>Order Online</b></a></li>
+                <li><a href="url"><b>Login</b></a></li>
             </ul>
         </nav>
     );
